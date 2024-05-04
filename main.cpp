b4f8f8b6e4854a90d1a804af17dd6d417ec77527
ShawnJhang <zzo30802@gmail.com>
2024-05-04 11:09:29 +0800

    [Feat] Implement test case and print result in main function

--- main.cpp
+++ main.cpp
@@ -78,7 +78,24 @@ public:
 };
 
 int main() {
-    cout << "hello world" << endl;
+    unordered_map<string, unordered_map<string, double>> rates = {
+        {"TWD", {{"TWD", 1}, {"JPY", 3.669}, {"USD", 0.03281}}},
+        {"JPY", {{"TWD", 0.26956}, {"JPY", 1}, {"USD", 0.00885}}},
+        {"USD", {{"TWD", 30.444}, {"JPY", 111.801}, {"USD", 1}}}
+    };
+
+    ExchangeRateService* exchangeRateService = new SimpleExchangeRateService(rates);
+    CurrencyExchangeService service(exchangeRateService);
+
+    // test input
+    vector<pair<string, string>> result;
+    result = service.convert("USD", "JPY", "1,525"); // 170,496.53
+    for (const auto& p : result) {
+        cout << p.first << " : " << p.second << endl;
+    }
+
+    // release
+    delete exchangeRateService;
 
     return 0;
 }
