aede0ef1299e74b414ec18bcf4478d4dd3277443
ShawnJhang <zzo30802@gmail.com>
2024-05-04 10:51:28 +0800

    [Feat] Implement ExchangeRateService interface
    This commit implements the ExchangeRateService interface, including the
    virtual function getExchangeRate.

--- main.cpp
+++ main.cpp
@@ -2,6 +2,12 @@
 
 using namespace std;
 
+class ExchangeRateService {
+public:
+    virtual double getExchangeRate(const string& source, const string& target) = 0;
+    virtual ~ExchangeRateService() {}
+};
+
 int main() {
     cout << "hello world" << endl;
 
