b35fc7db1ec78efdfb17c8521558a285de3b0a38
ShawnJhang <zzo30802@gmail.com>
2024-05-04 10:58:13 +0800

    [Feat] Implement SimpleExchangeRateService
    This commit implements the SimpleExchangeRateService, providing the
    concrete implementation for getExchangeRate.

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <unordered_map>
 
 using namespace std;
 
@@ -6,6 +7,18 @@ class ExchangeRateService {
 public:
     virtual double getExchangeRate(const string& source, const string& target) = 0;
     virtual ~ExchangeRateService() {}
+};
+
+class SimpleExchangeRateService : public ExchangeRateService {
+private:
+    unordered_map<string, unordered_map<string, double>> rates;
+
+public:
+    SimpleExchangeRateService(unordered_map<string, unordered_map<string, double>> rates) : rates(rates) {}
+
+    double getExchangeRate(const string& source, const string& target) override {
+        return rates[source][target];
+    }
 };
 
 int main() {
